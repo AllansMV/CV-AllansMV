b38aa213fdfebdff15316b2ffde93064c4fd1ea2
AllansMV <amejiavelasquez@gmail.com>
2022-03-22 17:18:35 -0600

    File 1.2

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+$(".hide"). on('click', function() {
+    $("nav>ul").toggle('slow')
+})
+
 function ver_cv(){
     location.href="https://drive.google.com/file/d/1teM0tyRBosLdeXtdnnL5LfScLpP1QMS-/view?usp=sharing"
 }
