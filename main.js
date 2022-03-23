1e12ab52ac0db91e4c15147ab134c4b2788b9abd
AllansMV <amejiavelasquez@gmail.com>
2022-03-22 19:52:15 -0600

    file 2

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ $(".hide"). on('click', function() {
 function ver_cv(){
     location.href="https://drive.google.com/file/d/1teM0tyRBosLdeXtdnnL5LfScLpP1QMS-/view?usp=sharing"
 }
+
+function b1(){
+    location.href="https://wa.me/50256104269?text=%C2%A1Hola!%20Espero%20que%20te%20encuentres%20muy%20bien.%20%C2%BFQu%C3%A9%20te%20trae%20por%20estos%20rumbos%3F"
+}
+
+function b2(){
+    location.href="https://www.instagram.com/allans_mv/"
+}
+
+function b3(){
+    location.href="mailto:amejiavelasquez@gmail.com"
+}
